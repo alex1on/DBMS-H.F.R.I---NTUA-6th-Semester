c7d93d10eafc41b74479349358d0633583b0f4db
alexion <alexandros.ionitsa@gmail.com>
2022-06-05 00:51:47 +0300

    Added page for each project

--- routes/project.js
+++ routes/project.js
@@ -3,5 +3,7 @@ const projectController = require('../controllers/project');
 const router = express.Router();
 
 router.get('/', projectController.getProjects);
+router.get('/info', projectController.getProjectInfo);
+router.post('/delete', projectController.postDeleteProject);
 
 module.exports = router;
