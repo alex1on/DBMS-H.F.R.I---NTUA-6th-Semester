08c4521286c5ac0228848c0a3731a9dffca4e47b
alexion <alexandros.ionitsa@gmail.com>
2022-06-05 00:50:21 +0300

    Added page for each project

--- controllers/project.js
+++ controllers/project.js
@@ -14,3 +14,42 @@ exports.getProjects = (req, res, next) => {
             .catch(err => console.log(err))
     })
 }
+
+exports.getProjectInfo = (req, res, next) => {
+    //const id = req.body.Project_id;
+    //const id = req.param('id');
+    const id = req.query.id;
+    pool.getConnection((err, conn) => {
+        var sqlQuery = `SELECT * FROM project WHERE Project_ID = ${id}`;
+
+        conn.promise().query(sqlQuery).then(([rows, fields]) => {
+            res.render('info.ejs', {
+                pageTitle: "Project Info",
+                fields: rows
+            })
+        })
+            .then(() => pool.releaseConnection(conn))
+            .catch(err => console.log(err))
+    })
+}
+
+/* Controller to delete grade by ID from database */
+exports.postDeleteProject = (req, res, next) => {
+    /* get id from params */
+    const id = req.query.id;
+
+    /* create the connection, execute query, flash respective message and redirect to grades route */
+    pool.getConnection((err, conn) => {
+        var sqlQuery = `DELETE FROM project WHERE Project_ID = ${id}`;
+
+        conn.promise().query(sqlQuery)
+            .then(() => {
+                pool.releaseConnection(conn);
+                res.redirect('/project');
+            })
+            .catch(err => {
+                res.redirect('/project');
+            })
+    })
+
+}
